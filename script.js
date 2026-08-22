d92be99be7c1d84ea1d3b21c182f2fb73209ff64
aparnaray06 <ray434575@gmail.com>
2026-08-22 09:52:50 +0530

    Install nodemon and apply on project

---
+++ script.js
@@ -0,0 +1,36 @@
+const mood = document.getElementById("mood");
+const generateBtn = document.getElementById("generateBtn");
+const prompt = document.getElementById("prompt");
+
+generateBtn.addEventListener("click", async ()=>{
+    const selectMood = mood.value;
+
+    if(!selectMood){
+        prompt.textContent = 'please select your mood';
+        return;
+    }
+    prompt.textContent = 'generating your journal prompt...';
+
+    try{
+        const response = await fetch('http://localhost:8000/journal-prompt',{
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                mood: selectMood
+            })
+        });
+        const data = await response.json()
+        if(!response.ok){
+                throw new Error(data.error || "something went wrong");
+            }
+            prompt.textContent = data.prompt;
+
+        }catch(error){
+        console.log(error);
+        prompt.textContent='unable to generate prompt. please try again.'
+    }
+})
+
+
---
+++ server.js
@@ -0,0 +1,64 @@
+import express, { json } from "express"
+
+import cors from "cors";
+
+import "dotenv/config"
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("server in running")
+})
+
+app.post("/journal-prompt", async (req, res) => {
+    try {
+        const { mood } = req.body;
+
+        if (!mood) {
+            return res.status(400).json({
+                error: "Mood is required"
+            })
+        }
+
+        const prompt = `write a short, encouraging journal prompt for someone feeling ${mood}`;
+
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.API_KEY}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    contents: [{
+                        parts: [{
+                            text: prompt
+                        }]
+                    }]
+                })
+            }
+        );
+        const data = await response.json();
+
+        if (!response.ok){
+            return res.status(response.status).json({
+                error: data.error?.message || `gemini api error`
+            });
+        }
+        const journalPrompt = data.candidates?.[0]?.content?.parts?.[0]?.text;
+
+        return res.json({ prompt: journalPrompt });
+    } catch(error){
+            console.log(error);
+            res.status(500).json({
+                error: 'something went wrong'
+            });
+    }
+});
+const PORT = 8000
+app.listen(PORT,()=>{
+    console.log(`server is running at PORT ${PORT}`);
+    
+});
